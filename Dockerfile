6cfa3f2f4514aa850bd9230e762f9d499d0b5c34
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2025-09-05 09:43:27 -0500

    ver

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-FROM python:3.10
+FROM python:3.10.16
 
 RUN apt update -y && \
 	apt install -y libgtk-3-dev && \
 	pip install --upgrade pip && \
 	pip install psychopy==2024.1.4
 
-ENV LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libffi.so.8
+ENV LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libffi.so.7
 
 
--- test_docker.sh
+++ test_docker.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Full run
 docker run \
     --mount type=bind,src=`pwd -P`/INPUTS,dst=/INPUTS \
 	--mount type=bind,src=`pwd -P`/OUTPUTS,dst=/OUTPUTS \
@@ -8,3 +9,12 @@ docker run \
 	/wkdir/fmri-ert-rift/src/convert-psydat.py \
 	--psydat_file /INPUTS/ert1.psydat \
 	--out_file /OUTPUTS/ert1.csv
+
+exit 0
+
+# Command line
+docker run -it --entrypoint bash \
+    --mount type=bind,src=`pwd -P`/INPUTS,dst=/INPUTS \
+	--mount type=bind,src=`pwd -P`/OUTPUTS,dst=/OUTPUTS \
+	--mount type=bind,src=`pwd -P`/..,dst=/wkdir \
+    psychopy-iis:test
