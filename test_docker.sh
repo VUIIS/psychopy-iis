a3a6cf6c13e13294c8aaa488679bd180c83b35cb
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2025-09-05 09:34:03 -0500

    tester

--- test_docker.sh
+++ test_docker.sh
@@ -3,7 +3,7 @@
 docker run \
     --mount type=bind,src=`pwd -P`/INPUTS,dst=/INPUTS \
 	--mount type=bind,src=`pwd -P`/OUTPUTS,dst=/OUTPUTS \
-	--mount type=bind,src=`pwd -P`,dst=/wkdir \
+	--mount type=bind,src=`pwd -P`/..,dst=/wkdir \
     psychopy-iis:test \
 	/wkdir/fmri-ert-rift/src/convert-psydat.py \
 	--psydat_file /INPUTS/ert1.psydat \
